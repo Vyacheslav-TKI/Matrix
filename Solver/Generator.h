98819445540863a6ff0d1bbce11346aaae358b96
I461-TKI <lukankinv20@gmail.com>
2024-12-18 02:10:51 +0300

    а

--- Solver/Generator.h
+++ Solver/Generator.h
@@ -4,8 +4,8 @@ namespace matrixx
 {
     template <typename T>
     /**
-     * @brief абстрактный класс Generator представл¤ет генератор значений.
-     * @tparam T “тип данных, которые будет генерировать класс-наследник.
+     * @brief абстрактный класс Generator представляет генератор значений.
+     * @tparam T тип данных, которые будет генерировать класс-наследник.
      */
     class Generator {
     public:
@@ -15,7 +15,7 @@ namespace matrixx
         virtual ~Generator() = default;
 
         /**
-         * @brief „чисто виртуальна¤ функци¤ для генерации значения типа T
+         * @brief чисто виртуальна¤ функция для генерации значения типа T
          * @return T —генерированное значение типа T.
          */
         virtual T generate() = 0;
